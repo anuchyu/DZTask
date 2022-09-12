0189ad1e9821fe630a036fec0a96dbe07b617e3d
Anuchina Yulia <yuliaanuchina@bk.ru>
2022-09-12 19:10:12 +0700

    Добавьте файлы проекта.

---
+++ DuplexLinledList.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+
+namespace DZTask
+{
+    public class DuplexLinledList<T> : IEnumerable<T>
+    {
+        public DuplexItem<T> Head { get; set; }
+        public DuplexItem<T> Tail { get; set; }
+        public int Count { get; set; }
+
+        public DuplexLinkedList() { }
+
+        public DuplexLinkedList(T data)
+        {
+            var item = new DuplexItem<T>(data);
+            Head = item;
+            Tail = item;
+            Count = 1;
+        }
+
+        public void Add(T data)
+        {
+            var item = new DuplexItem<T>(data);
+
+            if (Count == 0)
+            {
+                Head = item;
+                Tail = item;
+                Count = 1;
+                return;
+            }
+            Tail.Next = item;
+            item.Previous = Tail;
+            Tail = item;
+            Count++;
+        }
+
+        public void Delete(T data)
+        {
+            var current = Head;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    current.Previous.Next = current.Next;
+                    current.Next.Previous = current.Previous;
+                    Count--;
+                    return;
+                }
+                else current = current.Next;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿var duplexList = new DuplexLinkedList<int>();
+duplexList.Add(1);
+duplexList.Add(2);
+duplexList.Add(3);
+duplexList.Add(4);
+duplexList.Add(5);
+duplexList.Add(6);
+duplexList.Add(7);
+
+foreach (var item in duplexList)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine();
+
+duplexList.Delete(3);
+
+foreach (var item in duplexList)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine();
+
+var reverse = duplexList.Reverse();
+foreach (var item in reverse)
+{
+    Console.WriteLine(item);
+}
+
